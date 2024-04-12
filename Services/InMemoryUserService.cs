9726b8d54dac382b54fe77fa79cd8e3a2a477899
Cruz - Andrei - BSIT32E1 <161409141+Cruz-Andrei-BSIT32E1@users.noreply.github.com>
2024-04-12 23:10:30 +0800

    Validateuser

--- Services/InMemoryUserService.cs
+++ Services/InMemoryUserService.cs
@@ -9,7 +9,30 @@ namespace LoginApp.Services
             return users.FirstOrDefault(u => u.Username == username && u.Password == password);
         }
 
-
+        
+        public User RegisterUser(string name, string username, string email, string password)
+        {
+            var user = new User { Name = name, Username = username, Email = email, Password = password };
+            users.Add(user);
+            return user;
+        }
+        
+        public User? GetUserByUsername(string username)
+        {
+            return users.FirstOrDefault(u => u.Username == username);
+        }
+        
+        public User? UpdateUser(string name, string username, string email, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Username == username);
+            if (user != null)
+            {
+                user.Name = name;
+                user.Email = email;
+                user.Password = password;
+            }
+            return user;
+        }
 
         public User? GetUserByUsernameOrEmail(string username, string email)
         {
