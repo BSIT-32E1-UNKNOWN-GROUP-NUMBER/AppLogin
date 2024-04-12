b81419c85aca4fad18f7da2dc2bdb6dc98c7901a
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-04-12 23:07:39 +0800

    InMemory

---
+++ Services/InMemoryUserService.cs
@@ -0,0 +1,24 @@
+namespace LoginApp.Services
+{
+    public class InMemoryUserService : IUserService
+    {
+        private List<User> users = new List<User>();
+
+        public User? ValidateUser(string username, string password)
+        {
+            return users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        }
+
+
+
+        public User? GetUserByUsernameOrEmail(string username, string email)
+        {
+            return users.FirstOrDefault(u => u.Username == username || u.Email == email);
+        }
+
+        public void CreateUser(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
