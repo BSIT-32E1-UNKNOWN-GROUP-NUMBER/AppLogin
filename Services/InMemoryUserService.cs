50edf5cdb0129fa5ad3401d84da3be10d181121a
Pajarillo-TerrenceJay-BSIT32E1 <161125385+Pajarillo-TerrenceJay-BSIT32E1@users.noreply.github.com>
2024-04-13 00:03:16 +0800

    UpdateUser

--- Services/InMemoryUserService.cs
+++ Services/InMemoryUserService.cs
@@ -19,6 +19,17 @@ namespace AppLogin.Services
         {
             return users.FirstOrDefault(u => u.Username == username);
         }
+        public User? UpdateUser(string name, string username, string email, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Username == username);
+            if (user != null)
+            {
+                user.Name = name;
+                user.Email = email;
+                user.Password = password;
+            }
+            return user;
+        }
         public User? GetUserByUsernameOrEmail(string username, string email)
         {
             return users.FirstOrDefault(u => u.Username == username || u.Email == email);
